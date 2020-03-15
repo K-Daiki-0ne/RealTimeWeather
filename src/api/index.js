7b0a35c0c71b36d20050091a4a4d60baf3a4e54c
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-15 23:02:41 +0900

    Fix: add exception handling

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,11 @@ export const getWeather = async (lat, lon) => {
   const key = process.env.REACT_APP_API_KEY;
   const url = process.env.REACT_APP_API_URL;
   const reqUrl = `${url}/${key}/${lat},${lon}?lang=ja`;
-
-  const result = axios.get(reqUrl);
-  const data = result.data;
-  return data;
+  try {
+    const result = axios.get(reqUrl);
+    const data = result.data;
+    return data;
+  } catch (err) {
+    console.error(err)
+  }
 }
