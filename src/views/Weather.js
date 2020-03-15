8b0c79b370c941155c575ba0d6355ce6b34f47a8
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-15 22:48:04 +0900

    Create: api function & Add useState, useEffect

--- src/views/Weather.js
+++ src/views/Weather.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 
