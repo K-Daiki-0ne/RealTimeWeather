40fe34b057da6bfdda0063978de0cb5fd04ad412
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-15 22:38:51 +0900

    Create: Home container component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
 export const Home = () => {
   return (
-    <p>Home</p>
+    <Container maxWidth="sm">
+      <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '95vh' }}>
+        <p>Hello World</p>
+      </Typography>
+    </Container>
   )
 }
