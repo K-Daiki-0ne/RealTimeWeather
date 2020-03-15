597169f9826c68344bd70bb4922ea2778ae77073
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-15 23:01:07 +0900

    Create: api request function

---
+++ src/api/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const getWeather = async (lat, lon) => {
+  const key = process.env.REACT_APP_API_KEY;
+  const url = process.env.REACT_APP_API_URL;
+  const reqUrl = `${url}/${key}/${lat},${lon}?lang=ja`;
+
+  const result = axios.get(reqUrl);
+  const data = result.data;
+  return data;
+}
