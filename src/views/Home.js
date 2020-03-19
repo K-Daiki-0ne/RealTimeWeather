2b576aaf2d91078d3afc9e16c47786aae74e90d8
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-20 00:32:49 +0900

    Create: Home page

---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <Container fixed>
+      <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '95vh' }}>
+        <p>Hello World</p>
+      </Typography>
+    </Container>
+  )
+}
