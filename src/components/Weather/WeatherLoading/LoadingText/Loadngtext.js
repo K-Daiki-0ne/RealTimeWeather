d3122fbe2accb328e881c220640d2be7b92868d0
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-03-23 00:03:58 +0900

    Create: loadin page in weather

---
+++ src/components/Weather/WeatherLoading/LoadingText/Loadngtext.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LoadingText.css'
+
+export const LoadingText = () => {
+  // Reference(https://on-ze.com/archives/2524)
+  return (
+  <div className="loading">
+    <span>L</span>
+    <span>O</span>
+    <span>A</span>
+    <span>D</span>
+    <span>I</span>
+    <span>N</span>
+    <span>G</span>
+</div>
+  )
+}
--- src/components/Weather/WeatherLoading/WeatherLoading.js
+++ src/components/Weather/WeatherLoading/WeatherLoading.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { LoadingText } from './LoadingText/Loadngtext'
 import './WeatherLoading.css'
 
 export const WeatherLoading = () => {
   return (
-  <div class="container">
-    <div class="circles-wrapper">
-      <div class="circle circle-lg">
-        <div class="circle circle-md">
-          <div class="circle circle-sm"></div>
+  <div className="container">
+    <div className="load-container">
+      <div className="circles-wrapper">
+        <div className="circle circle-lg">
+          <div className="circle circle-md">
+            <div className="circle circle-sm"></div>
+          </div>
         </div>
       </div>
+    </div>
+    <div className="loading-text">
+      <LoadingText />
     </div>
   </div>
   )
