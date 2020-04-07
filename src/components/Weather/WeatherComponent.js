6acb5050f251f1a2fb4bc6ba5a59c325938f1bc7
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-07 23:57:23 +0900

    Fix: api request

--- src/components/Weather/WeatherComponent.js
+++ src/components/Weather/WeatherComponent.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getWeather } from '../../api/weather'
 import { WeatherLoading } from './WeatherLoading/WeatherLoading';
 
-export const WeatherComponent = () => {
+export const WeatherComponent = ({ location }) => {
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  let params = {}
+
+  useEffect(() => {
+    const data = window.location.search.substring(1)
+    data.split('&').forEach( param => {
+      const temp = param.split('=')
+      params = {
+        ...params,
+        [temp[0]]: temp[1]
+      }
+    })
+    console.log(params)
+    getWeather(params.lon, params.lat).then(data => setWeather(data))
+  }, [])
   return (
     <div className="weather-container">
       <WeatherLoading />
