aadb9e3a066b9ad1383321fd8858cc1605fa24c6
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-11 01:48:01 +0900

    Design: now time font

--- src/components/Weather/WeatherResult/WeatherResult.js
+++ src/components/Weather/WeatherResult/WeatherResult.js
@@ -4,10 +4,10 @@ import './WeatherResult.css'
 
 export const WeatherResult = ({ weatherResult }) => {
   const time = useTime()
-  console.log(time);
   return (
     <div className='result-container'>
       <h1 className='head'>Current Weather</h1>
+      <p className='now-time'>{time}</p>
       <h2 className='result-text'>{weatherResult.summary}</h2>
     </div>
   )
