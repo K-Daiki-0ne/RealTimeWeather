fbc59ac744135a63a37ebed796e27d8cbb52880e
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-16 01:33:25 +0900

    Fix: api function

--- src/api/weather.js
+++ src/api/weather.js
@@ -13,14 +13,8 @@ const key = process.env.REACT_APP_API_KEY;
  */
 export const getWeather = async (lon, lat) => {
   try {
-    const reqUrl = `${url}/${key}/${lat},${lon}`;
-    const result = await fetch(reqUrl, {
-      method: "GET", 
-      mode: "cors", 
-      header: {
-        'Access-Control-Allow-Origin': reqUrl
-      },
-    })
+    const url = `http://localhost:5000/${lat}/${lon}`;
+    const result = await fetch(url)
       .then(data => data.json());
     console.log('Get API ... OK');
     return result
