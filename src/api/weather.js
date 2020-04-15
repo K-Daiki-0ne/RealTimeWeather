34ef78739af7f19a4f2904df7487daaf6e1616fe
K-Daiki-0ne <kouguchi3456@yahoo.co.jp>
2020-04-16 01:34:36 +0900

    Delete: comment

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,16 +1,5 @@
-// import axios from 'axios';
-
 const url = process.env.REACT_APP_API_URL;
 const key = process.env.REACT_APP_API_KEY;
-
-/**  
- * This code happens cors error .
- * I use CORS Unblock(Chrome web tool (https://chrome.google.com/webstore/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino/related?hl=ja)) .
- * But don't use because it is dangerrous .
- * I create this app because this app is my portfolio .
- * Correct as soon as the cause is known .
- * If you want to use this app that you use this app on one's own responsibility .
- */
 export const getWeather = async (lon, lat) => {
   try {
     const url = `http://localhost:5000/${lat}/${lon}`;
